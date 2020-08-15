d2432db065d01fa0d1805d533cf0929d1f956bb1
Robert Moore <rcmoore38@gmail.com>
2020-08-15 17:36:12 +0100

    feat(server): cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,6 +15,10 @@ export async function main(event, context, callback) {
     });
     const response = {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
       body: JSON.stringify(sortedBySalary),
     };
     callback(null, response);
